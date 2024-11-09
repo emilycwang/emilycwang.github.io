52f44e3376bfc122c5ede6f32283db7c42e3604a
emilycwang <emily.wang.2022@gmail.com>
2024-11-09 13:16:42 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "emilycwang.github.io/"
+  base: "/emilycwang.github.io/"
 })
