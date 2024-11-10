0879095017e044671aa0fd929dd449013bfe0637
Emmy <137639991+emilycwang@users.noreply.github.com>
2024-11-09 21:59:25 -0600

    manual file upload

---
+++ src/assets/icons/index.js
@@ -0,0 +1,41 @@
+import css from './css.svg'
+import RStudio from './RStudio.svg'
+import Jupyter from './Jupyter.svg'
+import MySQL from './MySQL.svg'
+import Python from './Python.svg'
+import qgis from './qgis.svg'
+import Stata from './Stata.svg'
+import arc from './arc.svg'
+import excel from './excel.svg'
+import github from './github.svg'
+import html from './html.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import factory from './factory.png'
+import violin from './violin.png'
+import testing from './testing.png'
+import game from './game.png'
+
+export {
+    css,
+    arc,
+    Jupyter,
+    MySQL,
+    Python,
+    qgis,
+    Stata,
+    RStudio,
+    excel,
+    github,
+    html,
+    linkedin,
+    twitter,
+    arrow,
+    contact,
+    violin,
+    factory,
+    testing,
+    game
+}
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import CTA from "./CTA";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Alert from "./Alert";
+
+export {
+    Alert,
+    CTA,
+    Loader,
+    Navbar,
+    Footer,
+}
