969c89be5114cb0b249c054303aadf6708e31b15
emilycwang <emily.wang.2022@gmail.com>
2024-11-09 22:39:24 -0600

    last update clear exit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "/emilycwang.github.io/"
+  base: "https://emilycwang.github.io/"
 })
